c991d1c5895114fe1f434cc02ccd0524be7cec83
Adi1020 <adis0240@gmail.com>
2024-06-16 18:07:57 +0300

    Backend_#1

--- lift_CRUD.py
+++ lift_CRUD.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate()
 firebase_admin.initialize_app(cred)
 
 # Create Firestore client
